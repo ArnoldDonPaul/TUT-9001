b5c2e79408715dbe1b73aecd059d1454f8278a77
Arnold <arnolddonpaul@gmail.com>
2020-03-16 17:31:44 -0400

    status of checkbox is able to console.log

--- tut9001/client/src/components/InputForm/InputForm.jsx
+++ tut9001/client/src/components/InputForm/InputForm.jsx
@@ -10,11 +10,28 @@ export default class Greeting extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            showInfo: false
+            showInfo: false,
+            boxUpper: false,
+            boxLower: false,
+            boxYes: false
         };
         this.handleSubmit = this.handleSubmit.bind(this);
     }
-
+    handleChange1 = event => {
+        this.setState({ boxUpper: event.target.checked }, () => {
+            console.log("This returned true or false", this.state.boxUpper);
+        });
+    }
+    handleChange2 = event => {
+        this.setState({ boxLower: event.target.checked }, () => {
+            console.log("This returned true or false", this.state.boxLower);
+        });
+    }
+    handleChange3 = event => {
+        this.setState({ boxYes: event.target.checked }, () => {
+            console.log("This returned true or false", this.state.boxYes);
+        });
+    };
     handleSubmit(event) {
         event.preventDefault();
         this.setState({
@@ -27,20 +44,20 @@ export default class Greeting extends Component {
                 <div className='greeting'>
                     <img className='TUT-greeting' src={TUT} alt='Daisy...Daisy...'></img>
                     <div className='terminal'>
-                        <p className='terminal__text'>>> SIMPLY COMPLETE THE FOLLOWING...</p>
-                        <p className='terminal__text'>>> WHAT'S YOUR FOCUS FOR TODAY?</p>
+                        <p className='terminal__text'>>> PLEASE COMPLETE THE FOLLOWING...</p>
+                        <p className='terminal__text'>>> WHAT IS YOUR FOCUS FOR TODAY?</p>
                         <form className='checkbox-form' onSubmit={this.handleSubmit}>
                             <div className='option-box'>
-                                <input type="checkbox" className='option-box__box' name="upper" value="upper"></input>
+                                <input type="checkbox" className='option-box__box' name="upper" value="upper" onChange={this.handleChange1}></input>
                                 <label for="upper" className='option-box__text'> UPPER BODY</label>
                             </div>
                             <div className='option-box'>
-                                <input type="checkbox" className='option-box__box' name="lower" value="upper"></input>
+                                <input type="checkbox" className='option-box__box' name="lower" value="lower" onChange={this.handleChange2}></input>
                                 <label for="upper" className='option-box__text'> LOWER BODY</label>
                             </div>
                             <p className='terminal__text'>>> DO YOU HAVE ACCESS TO DUMBBELLS?</p>
                             <div className='option-box'>
-                                <input type="checkbox" className='option-box__box' name="yes" value="yes"></input>
+                                <input type="checkbox" className='option-box__box' name="yes" value="yes" onChange={this.handleChange3}></input>
                                 <label for="yes" className='option-box__text'> YES</label>
                             </div>
                             <p className='terminal__text'>>> READY TO RECEIVE YOUR WORKOUT?</p>
