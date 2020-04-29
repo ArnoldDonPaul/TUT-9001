c99cadf1107a167f387e5dad7937e809c737d388
Jay Gadi <jay@soapboxhq.com>
2020-04-29 18:59:07 -0400

    add about component

--- tut9001/client/src/components/LandingPage/LandingPage.jsx
+++ tut9001/client/src/components/LandingPage/LandingPage.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import Power from '../../assets/power.png';
 import TUT from '../../components/TUT/TUT'
+import About from '../About/About';
 
 import './LandingPage.scss'
 
@@ -13,11 +14,7 @@ export default class LandingPage extends Component {
                 <Link to='/boot'>
                     <img className='nav__power' src={Power} alt='Scanner'></img>
                 </Link>
-                <div className='output'>
-                    <h1 className='output__about'>ABOUT</h1>
-                    <p className='output__about'>TUT-9001 AIMS TO DELIVER SIMPLIFIED WORKOUTS THAT GUIDE THE USER THROUGH BASIC PATTERNS OF MOVEMENT.</p>
-                    <p className='output__about'>TUT STANDS FOR "TIME UNDER TENSION", SO IT DOESNT MATTER HOW MANY REPITITIONS ARE COMPLETED - RATHER, TUT-9001 ENSURES THE USER IS MAINTAINING TENSION THROUGHOUT THE MUSCLE FOR AN APPROPRIATE AMOUNT OF TIME. </p>
-                </div>
+                <About />
             </section >
         )
     }
--- tut9001/client/src/components/PowerDown/PowerDown.jsx
+++ tut9001/client/src/components/PowerDown/PowerDown.jsx
@@ -4,6 +4,7 @@ import Power from '../../assets/power.png';
 import TUT from '../TUT/TUT';
 
 import './PowerDown.scss';
+import About from '../About/About';
 
 export default class PowerDown extends Component {
     render() {
@@ -13,11 +14,7 @@ export default class PowerDown extends Component {
                 <Link to='/boot'>
                     <img className='nav__power' src={Power} alt='Power'></img>
                 </Link>
-                <div className='output'>
-                    <h1 className='output__about'>ABOUT</h1>
-                    <p className='output__about'>TUT-9001 AIMS TO DELIVER SIMPLIFIED WORKOUTS THAT GUIDE THE USER THROUGH BASIC PATTERNS OF MOVEMENT.</p>
-                    <p className='output__about'>TUT STANDS FOR "TIME UNDER TENSION", SO IT DOESNT MATTER HOW MANY REPITITIONS ARE COMPLETED - RATHER, TUT-9001 ENSURES THE USER IS MAINTAINING TENSION THROUGHOUT THE MUSCLE FOR AN APPROPRIATE AMOUNT OF TIME. </p>
-                </div>
+                <About />
             </section>
         )
     }
